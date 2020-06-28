217ee1ea40ef022fa273d2159122bb4953da4a39
Jonghyun Kim <jhkim@dit.ac.kr>
2020-06-28 12:06:21 +0900

    add Object push

--- app/src/main/java/com/example/firebasetest2/MainActivity.java
+++ app/src/main/java/com/example/firebasetest2/MainActivity.java
@@ -1,6 +1,9 @@
+// https://console.firebase.google.com/project/fir-test2-ad6d3/database/fir-test2-ad6d3/data
+
 package com.example.firebasetest2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,16 +11,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase db = FirebaseDatabase.getInstance ();
     DatabaseReference myRef = db.getReference ("message");
+//    DatabaseReference myRef = db.getReference ();
+
     TextView textView;
     EditText editText;
 
@@ -29,62 +38,41 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById (R.id.textView);
         editText = (EditText)findViewById(R.id.inputText);
-
-//        Button btnListener = (Button) findViewById(R.id.button);
-
-        //myRef.setValue ("Hello Firebase");
-
-//        btnListener.setOnClickListener (new View.OnClickListener () {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText (MainActivity.this, "Click event occured", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-//        myRef.addListenerForSingleValueEvent (new ValueEventListener () {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                String val = (String) dataSnapshot.getValue ();
-//                textView.setText (val);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
     }
 
     public void put_btn_Click(View view)
     {
-//        TextView textView = (TextView)findViewById(R.id.textView);
-//        EditText editText = (EditText)findViewById(R.id.inputText);
         String inData = editText.getText().toString ();
-
-        //textView.setText(editText.getText());
-        //textView.setText (myRef.getKey ())
-        myRef.setValue(inData);
+        //myRef.setValue(inData);
+        //myRef.push.setValue(inData);
+        myRef.push().child("test").setValue(inData);
         editText.setText ("");
     }
 
     public  void get_btn_Click(View view) {
-        String getData = myRef.getKey ();
-        textView.setText (getData);
 
-        myRef.addListenerForSingleValueEvent (new ValueEventListener () {
+        myRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String val = (String) dataSnapshot.getValue ();
-                textView.setText (val);
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String data = dataSnapshot.getValue().toString();
+                textView.append (data.toString() + "\n");
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
         });
-
     }
-
-
 }
